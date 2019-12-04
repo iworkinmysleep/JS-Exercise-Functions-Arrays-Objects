84ecd70ce516d66c899491d19d703a26151e632f
Jason Hinton <jason_hinton@att.net>
2019-12-03 23:10:40 -0500

    first commit

--- index.js
+++ index.js
@@ -39,8 +39,13 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  return{
+    id: id,
+    name: name,
+    email: email
+  };
+
 }
 
 /**
